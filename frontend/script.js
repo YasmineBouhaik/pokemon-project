4b8cd98a7c406accaf8b0a407363d1094b623b71
charly <charly.dewalles@gmail.com>
2022-04-05 12:15:27 +0200

    base frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,35 +1,14 @@
 
-
-fetch('https://pokeapi.co/api/v2/pokemon?limit=25')
+fetch('http://localhost:8080/pokemon')
 .then((response) => response.json())
 .then((response) => {
-
-    let random = Math.random() * (25 - 0) + 0;
-    let resultOfRandom = 0; 
-
+    let random = Math.random() * 26 ;
     for(i = 0; i < random; i++){
-        resultOfRandom = response.results.lastIndexOf(response.results[i]);
+        const imageUrl = getPokemonImage(response[i].number_)
+        document.getElementById('poke').setAttribute('src', imageUrl);
     }
-
-    
-    const number = getPokemonNumber(response.results[resultOfRandom].url)
-    const finalUrl = getPokemonImage(number)
-    console.log(getPokemonImage(number));
-    console.log(response.results.url);
-    console.log(response.results[resultOfRandom]);
-    
-    let poke = document.getElementById('poke').setAttribute('src', finalUrl);
-
-    
 }); 
-
-
 
 function getPokemonImage(number){
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${number}.png`
-}
-
-function getPokemonNumber (url){
-    const regex = /(\d+)\/$/; 
-    return  (url.match(regex) || [])[1]; 
 }
--- src/main/java/com/pokemon/pokemon/project/model/PokemonDressed.java
+++ src/main/java/com/pokemon/pokemon/project/model/PokemonDressed.java
@@ -20,28 +20,17 @@ public class PokemonDressed {
 	@EmbeddedId
 	private PrimaryKeyPokemonDressed id = new PrimaryKeyPokemonDressed();
 	
-//	@Column (name = "pokemon_id")
-//	private Long pokemon_id;
-//
-//	@Column (name = "dresseur_id")
-//	private Long dresseur_id;
 
 	@Column(name = "nickname")
 	private String nickname;
 	
 	
 	public PokemonDressed(PrimaryKeyPokemonDressed id, Long pokemon_id, Long dresseur_id, String nickname) {
-		//super();
 		this.id = id;
-//		this.pokemon_id = pokemon_id;
-//		this.dresseur_id = dresseur_id;
 		this.nickname = nickname;
 	}
 
 
-	public PokemonDressed() {
-		//super();
-	}
 	
 	public PrimaryKeyPokemonDressed getId() {
 		return id;
